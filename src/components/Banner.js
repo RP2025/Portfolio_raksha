f9abc41274777b0716a204b736bd740970c9cd3d
rakshapahariya <rakshapahariya1208@gmail.com>
2023-06-18 04:04:35 +0530

    DAY 1

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,55 @@
 import React from 'react';
+import Img from '../assets/avatar.png'
+
+import {FaGithub ,FaLinkedinIn , FaMedium} from 'react-icons/fa'
+import {TypeAnimation} from 'react-type-animation'
+import {motion} from 'framer-motion'
+import {fadeIn} from '../variants'
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return <section id = 'home' className='section'>
+    <div className='container mx-auto'>
+      <div>
+        <div>
+          <h1>
+            RAKSHA  <span>PAHARIYA</span>
+          </h1>
+          <div className='mb-6 text-[46px] lg:text-[60px] font-secondary font-bold uppercase leading-[1]'>
+            <span className='text-white mi-4'>I am a </span>
+            <TypeAnimation sequence={[
+              'Software Developer' , 
+              2000, 
+              'Designer' , 
+              2000, 
+              'Coder', 
+              2000 ,
+              'ML enthusiastic', 
+              2000 ,
+              'Philomath' , 
+              2000]
+              }
+             speed={30} 
+             className='text-accent'
+             wrapper='span'
+             repeat={Infinity}
+            />
+          </div>
+          <div>
+            <p>
+            I am currently a Pre Final Year Undergraduate pursuing my B.tech degree in Computer Science and Engineering from the Indian Institute of Engineering Science and Technology, Shibpur.
+            I am a passionate to dive deep in tech world. You'll often find me immersed in the melodies of old hits during my free time.
+
+            Happy Coding!!
+            </p>
+          </div>
+        </div>
+        <div>
+          <img src={Img} alt='' />
+        </div>
+      </div>
+    </div>
+  
+  </section>;
 };
 
 export default Banner;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
+import Logo from '../assets/logo.png'
 const Header = () => {
-  return <div>Header</div>;
+  return <header className='py-8'>
+    <div className='container mx-auto'>
+      <div className='flex justify-between items-center'>
+        {/* logo */}
+        <a href='#'>
+          <img src={Logo} alt="logo" width={200} height={200} />
+        </a>
+        {/* button */}
+        <button className='btn btn-sm'> Contact Me </button>
+
+      </div>
+    </div>
+  
+  </header>;
 };
 
 export default Header;
