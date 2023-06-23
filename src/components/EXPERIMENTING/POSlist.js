0ae4a28e95cad5775377345e5b7162d513ee020b
rakshapahariya <rakshapahariya1208@gmail.com>
2023-06-23 20:02:02 +0530

    DAY 3 -more  BUGS FIXED

--- src/components/EXPERIMENTING/POSlist.js
+++ src/components/EXPERIMENTING/POSlist.js
@@ -4,15 +4,15 @@ import { List } from 'semantic-ui-react'
 const ListExampleHeader = () => (
   <List className='container mx-auto text-[20px]'>
     <List.Item>
-        <List.Header className='flex items center font-bold'> -> Member at CodeIIEST</List.Header>
+        <List.Header className='flex items center font-bold'> -- Member at CodeIIEST</List.Header>
        Core Member of official Coding Club of IIEST Shibpur 
     </List.Item>
     <List.Item>
-        <List.Header className='flex items center font-bold'> -> Member at GDSC IIEST</List.Header>
+        <List.Header className='flex items center font-bold'> -- Member at GDSC IIEST</List.Header>
        Core Member of GDSC IIEST Shibpur
     </List.Item>
     <List.Item>
-        <List.Header className='flex items center font-bold'> -> Entrepreneur Development Cell , IIEST</List.Header>
+        <List.Header className='flex items center font-bold'> -- Entrepreneur Development Cell , IIEST</List.Header>
        Core Member of Entrepreneur Development Cell , IIEST 
     </List.Item>
   </List>
